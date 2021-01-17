a7f8bc27893e08323506e01083099287b712888a
Yan Waipann <yanwaipann@gmail.com>
2021-01-17 19:38:03 +0630

    Add schema for dumping user info

--- schemas/User.py
+++ schemas/User.py
@@ -1,7 +1,24 @@
 from ma import ma
+from marshmallow import fields
 from models.User import UserModel
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
+
+
+class SaveUserAnimeSchema(ma.Schema):
+    class Meta:
+        fields = ("anime_id",)
+
+
+class DumpUserInfoSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "name",
+            "username",
+            "email",
+            "role",
+            "joined",
+        )
