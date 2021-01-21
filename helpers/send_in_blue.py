c177ed5535f242e6fec8217078250bb48546e628
Yan Waipann <yanwaipann@gmail.com>
2021-01-21 16:38:21 +0630

    Add method to send pw reset email

--- helpers/send_in_blue.py
+++ helpers/send_in_blue.py
@@ -59,3 +59,47 @@ class SendInBlue:
         except Exception as ex:
             print(ex)
             raise SendInBlueError(cls.REQUEST_ERROR)
+
+    @classmethod
+    def send_pw_reset_email(cls, **kwargs) -> None:
+        # The code is not dry! Re UPDATE IT!!
+        payload = {
+            "sender": {
+                "name": "MyanNime",
+                "email": "noreply@myannime.com"
+            },
+            "to": [
+                {
+                    "email": kwargs["email"],
+                    "name": kwargs["name"]
+                }
+            ],
+            "params": {
+                "NAME": kwargs["name"],
+                "PW_RESET_LINK": kwargs["pw_reset_link"]
+            },
+            "templateId": 2
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "api-key": cls.API_KEY
+        }
+
+        try:
+            response = requests.request(
+                "POST",
+                cls.SEND_EMAIL_ENDPOINT,
+                json=payload,
+                headers=headers
+            )
+
+            print(response.status_code)
+            if response.status_code != 201:
+                raise SendInBlueError(cls.MAIL_NOT_SENT_ERROR)
+
+            print(response.text)
+
+        except Exception as ex:
+            print(ex)
+            raise SendInBlueError(cls.REQUEST_ERROR)
