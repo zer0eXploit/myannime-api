3869fe1e03a39cbea01e5d48b8235db6ec2f202f
Yan Waipann <yanwaipann@gmail.com>
2021-01-26 14:11:07 +0630

    Set propagate exception to True

--- default_config.py
+++ default_config.py
@@ -6,3 +6,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = os.environ["JWT_SECRET"]
 UPLOADED_IMAGES_DEST = os.path.join("static", "images")
 APP_SECRET = os.environ["APP_SECRET"]
+PROPAGATE_EXCEPTIONS = True
