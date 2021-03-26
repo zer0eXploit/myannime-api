84b06ea94453048d91f6c136a3e770f68dab3936
Yan Waipann <yanwaipann@gmail.com>
2021-03-26 13:18:13 +0630

    Add frontend and api docs uri in response

--- resources/Root.py
+++ resources/Root.py
@@ -8,4 +8,8 @@ from helpers.strings import get_text
 class Root(Resource):
     @classmethod
     def get(cls) -> Dict:
-        return {"greetings": get_text('home_welcome')}, 200
+        return {
+            "greetings": get_text('home_welcome'),
+            "frontend_uri": get_text('frontend_uri'),
+            "api_docs_uri": get_text('api_docs_uri')
+        }, 200
